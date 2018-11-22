512edd30a717f16ad44c9905700d3135e7c50550
Christian <christianbroe@outlook.com>
2018-11-22 12:08:07 +0100

    Added UnitTestRender

--- ATM/ATM.Test.Unit/RenderUnitTest.cs
+++ ATM/ATM.Test.Unit/RenderUnitTest.cs
@@ -35,12 +35,14 @@ namespace ATM.Test.Unit
 
         }
 
-        [Test]
-        public void Initial_TrackUpdatedOneTrack_StringIsCorrect()
-        {
+        [TestCase("NIC007", 27272, 67676, 14141)]
+        [TestCase("CBN007", 25252, 66666, 11111)]
+        [TestCase("BON007", 26262, 68545, 12458)]
+        public void Initial_TrackUpdatedOneTrack_StringIsCorrect(string tag, int x, int y, int alt)
+        {       
             List<Track> updatedTracklistToTest = new List<Track>();
             List<IEvent> evnetList = new List<IEvent>();
-            updatedTracklistToTest.Add(new Track() { Tag = "NIC111", X = 25000, Y = 65000, Altitude = 15000, Velocity = 10, Course = 45, TimeStamp = DateTime.ParseExact("20151006213456789", "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture) });
+            updatedTracklistToTest.Add(new Track() { Tag = tag, X = x, Y = y, Altitude = alt, Velocity = 10, Course = 45, TimeStamp = DateTime.ParseExact("20151006213456789", "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture) });
 
             TracksUpdatedEventArgs args = new TracksUpdatedEventArgs(updatedTracklistToTest, evnetList);
             _updater.TracksUpdated += Raise.EventWith(args);
@@ -48,7 +50,7 @@ namespace ATM.Test.Unit
             bool isInList = false;
             for (int i = 0; i < _outputList.Count; i++)
             {
-                if (_outputList[i].Contains("NIC111"))
+                if (_outputList[i].Contains(tag))
                 {
                     isInList = true;
                 }
