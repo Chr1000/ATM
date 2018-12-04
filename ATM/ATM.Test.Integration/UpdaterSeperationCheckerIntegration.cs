f182c8945675037fa85f6fc4eea04185d2fbb081
Niclas Spas <niclasspas@gmail.com>
2018-12-04 05:27:00 +0100

    Added integrationtest SeperationStopEvent_is_raised_on_event2

--- ATM/ATM.Test.Integration/UpdaterSeperationCheckerIntegration.cs
+++ ATM/ATM.Test.Integration/UpdaterSeperationCheckerIntegration.cs
@@ -122,5 +122,49 @@ namespace ATM.Test.Integration
             Assert.That(_raisedEvent.Track, Is.EqualTo(track2));
             Assert.That(_raisedEvent.ConflictingTrack, Is.EqualTo(track1));
         }
+
+        [Test]
+        public void SeperationStopEvent_is_raised_on_event2()
+        {
+
+            Track track1 = new Track()
+            {
+                Tag = "JHG654",
+                X = 30000,
+                Y = 30000,
+                Altitude = 5000,
+                TimeStamp = DateTime.ParseExact("20151006213456789", "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture)
+            };
+            _activeTracks.Add(track1);
+
+            Track track2 = new Track()
+            {
+                Tag = "NIC888",
+                X = 30000,
+                Y = 30000,
+                Altitude = 4800,
+                TimeStamp = DateTime.ParseExact("20151006213456800", "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture)
+            };
+
+            Track track2updated = new Track()
+            {
+                Tag = "NIC888",
+                X = 30000,
+                Y = 30000,
+                Altitude = 4000,
+                TimeStamp = DateTime.ParseExact("20151006213456800", "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture)
+            };
+            _activeTracks.Add(track2updated);
+
+            IEvent alertEvent = new Event(_activeEvents, "SEPERATION ALERT", track1, DateTime.Now, track2, _seperationChecker);
+            _activeEvents.Add(alertEvent);
+
+            SeperationCheckerEventArgs SCEargs = new SeperationCheckerEventArgs(_activeEvents, _activeTracks, track2updated);
+
+            _updater.SeperationChecker += Raise.EventWith(this, SCEargs);
+
+            Assert.That(_raisedEvent.Track, Is.EqualTo(track1));
+            Assert.That(_raisedEvent.ConflictingTrack, Is.EqualTo(track2));
+        }
     }
 }
