cbd533931386273cc5031668ef762033d77482a8
Niclas Spas <niclasspas@gmail.com>
2018-12-04 17:52:59 +0100

    Added UnitTests covering all of Parser.ReadTrackData

--- ATM/ATM.Test.Unit/TrackParserUnitTest.cs
+++ ATM/ATM.Test.Unit/TrackParserUnitTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,9 +26,42 @@ namespace ATM.Test.Unit
             
             );
         }
+
+        [Test]
         public void TrackParserTestOfTag_TagIsCorrect()
         {
-            NUnit.Framework.Assert.That(receivedTrackData[0].Tag, Is.EqualTo("SKK444"));
+            string correctTag = "SKK444";
+            NUnit.Framework.Assert.That(receivedTrackData[0].Tag, Is.EqualTo(correctTag));
         }
+
+        [Test]
+        public void TrackParserTestOfX_XIsCorrect()
+        {
+            int correctX = 12345;
+            NUnit.Framework.Assert.That(receivedTrackData[0].Tag, Is.EqualTo(correctX));
+        }
+
+        [Test]
+        public void TrackParserTestOfY_YIsCorrect()
+        {
+            int correctY = 67890;
+            NUnit.Framework.Assert.That(receivedTrackData[0].Tag, Is.EqualTo(correctY));
+        }
+
+        [Test]
+        public void TrackParserTestOfAtlitude_AltitudeIsCorrect()
+        {
+            int correctAltitude = 10000;
+            NUnit.Framework.Assert.That(receivedTrackData[0].Tag, Is.EqualTo(correctAltitude));
+        }
+
+        [Test]
+        public void TrackParserTestOfTimeStamp_TimeStampIsCorrect()
+        {
+            DateTime correctTimeStamp = DateTime.ParseExact("20181004123456789", "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture);
+            NUnit.Framework.Assert.That(receivedTrackData[0].Tag, Is.EqualTo(correctTimeStamp));
+        }
+
+
     }
 }
